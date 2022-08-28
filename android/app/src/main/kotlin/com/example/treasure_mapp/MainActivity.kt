a74cdec45072545ea23b2bdc3de43c11d6e2fa4f
Ahmed2120 <ahmed202ashraf202@gmail.com>
2022-08-28 10:29:06 +0200

    check internet connection

--- android/app/src/main/kotlin/com/example/treasure_mapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/treasure_mapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.treasure_mapp
+package ahmad.ashraf.treasure_map
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/screens/main_map_screen.dart
+++ lib/screens/main_map_screen.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:provider/provider.dart';
 
 import '../services/ad_mob_service.dart';
 import '../services/controller.dart';
 import '../services/dbhelper.dart';
+import '../widgets/msg_text.dart';
 import 'manage_place.dart';
 import '../model/place.dart';
 import '../widgets/place_dialog.dart';
@@ -25,13 +29,40 @@ class _MainMapState extends State<MainMap> {
   bool loader = false;
   BannerAd? _bannerAd;
 
+  bool _isRefused = false;
+  String internetMsg = '';
+  bool hasInternet = false;
+  late StreamSubscription internetCon;
+
   @override
   void initState() {
     super.initState();
+    internetCon = InternetConnectionChecker().onStatusChange.listen((event) {
+      final hasInter = event == InternetConnectionStatus.connected;
+      print('connected 2: $hasInternet');
+      if (hasInter) {
+        setState(() {
+          _isRefused = false;
+          internetMsg = '';
+        });}
+    else{
+        setState(() {
+          _isRefused = true;
+          internetMsg = 'NO Internet Connection';
+        });
+      }
+      });
     getPosition();
     Provider.of<Controller>(context, listen: false).getData();
 
     _createBannerAd();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    internetCon.cancel();
   }
 
   void getPosition() {
@@ -71,13 +102,13 @@ class _MainMapState extends State<MainMap> {
           IconButton(
               onPressed: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ManagePlaces()));
+                    MaterialPageRoute(builder: (context) => const ManagePlaces()));
               },
               icon: const Icon(Icons.list))
         ],
       ),
-      body: loader
-          ? Center(child: CircularProgressIndicator())
+      body: _isRefused ? MsgText(msg: internetMsg) : loader
+          ? const Center(child: CircularProgressIndicator())
           : Container(
         child: GoogleMap(
           initialCameraPosition: position,
@@ -85,10 +116,10 @@ class _MainMapState extends State<MainMap> {
           Set<Marker>.of(Provider.of<Controller>(context).markers),
         ),
       ),
-      floatingActionButton: Align(
-        alignment: Alignment(0.7, 1),
+      floatingActionButton: _isRefused ? Container() : Align(
+        alignment: const Alignment(0.7, 1),
         child: FloatingActionButton(
-          backgroundColor: Color(0xFF827773),
+          backgroundColor: const Color(0xFF827773),
           child: const Icon(Icons.add_location),
           onPressed: () {
             int here = Provider.of<Controller>(context, listen: false)
